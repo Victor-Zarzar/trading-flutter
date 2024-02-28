0c8bcfaeba76e3f791f6311f08ed20b0cc853e8b
victorzarzar <victorzarzar58@gmail.com>
2024-02-28 06:24:18 -0300

    implement assets, and create page home page, updates in main, and create components assets_app, app_theme

---
+++ lib/components/assets_app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ImageHome {
+  ImageHome._();
+
+  static Image asset() {
+    return Image.asset('lib/assets/images/usd.png');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trading_app_flutter/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyApp(),
+      home: const HomePage(),
     );
   }
 }
