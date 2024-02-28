f91657eb57bcf24b8cc3082bb92b25b25bf24faa
victorzarzar <victorzarzar58@gmail.com>
2024-02-28 06:55:00 -0300

    implement packages, and updates in homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,61 @@
 import "package:flutter/material.dart";
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.topLeft,
+          colors: [
+            Colors.blue.shade900,
+            Colors.blue.shade300,
+            Colors.blue.shade900,
+            Colors.blue.shade200,
+          ],
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SafeArea(child: SizedBox(
+              height: MediaQuery.of(context).size.height / 2.5,
+              child: Column(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 25),
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(100))
+                      ),
+                      child: IconButton(onPressed: () {},
+                      icon: const Icon(Icons.menu, color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ),
+            Expanded(child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(25),
+              decoration: const BoxDecoration(color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(65),
+                topRight: Radius.circular(65),
+              ),
+              ),
+            ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
